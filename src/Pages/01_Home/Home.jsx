c963532d9ad8a1c63d6eb0f44ea389c250e13478
Zlaam <mrzalaam@gmail.com>
2023-12-31 22:48:41 +0500

    last stable commit

--- src/Pages/01_Home/Home.jsx
+++ src/Pages/01_Home/Home.jsx
@@ -3,6 +3,7 @@ import "./Home.scss";
 import Home_Section_2 from "./home_section_2/Home_section_2";
 import Home_section_3 from "./home_section_3/home_section_3";
 import Home_Section_4 from "./home_section_4/Home_section_4";
+import Home_section_5 from "./home_section_5/home_section_6";
 const Home = () => {
   return (
     <>
@@ -15,6 +16,7 @@ const Home = () => {
         <Home_Section_2 />
         <Home_Section_4 />
         <Home_section_3 />
+        <Home_section_5 />
       </div>
     </>
   );
--- src/Pages/01_Home/home_section_5/home_section_6.jsx
+++ src/Pages/01_Home/home_section_5/home_section_6.jsx
@@ -1,9 +1,34 @@
-import {} from "react";
+import {
+  Accordion,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel,
+} from "react-accessible-accordion";
+import "react-accessible-accordion/dist/fancy-example.css";
+import "./Home_section_5.scss";
+import items from "./accordian.json";
 
 const Home_section_5 = () => {
   return (
     <>
-      <div>HOme section</div>
+      <div className="home_section_5">
+        <h1>{"FAQ'S"}</h1>
+        <Accordion allowZeroExpanded={false} className="accordion_container">
+          {items.map((item) => (
+            <AccordionItem key={item.id}>
+              <AccordionItemHeading>
+                <AccordionItemButton className="question">
+                  {item.question}
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel>
+                <p className="answer">{item.answer}</p>
+              </AccordionItemPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </>
   );
 };
