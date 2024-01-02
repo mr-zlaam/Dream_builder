ac5dccd3b68d153dfcd520914590f19c4a4699e0
Zlaam <mrzalaam@gmail.com>
2024-01-02 13:32:49 +0500

    last stable commit

--- src/Pages/02_Services/DynamicPages/ArchitecturalPage.jsx
+++ src/Pages/02_Services/DynamicPages/ArchitecturalPage.jsx
@@ -3,7 +3,102 @@ import "./ArchitecturalPage.scss";
 const ArchitecturalPage = () => {
   return (
     <>
-      <div> Arch page</div>
+      <div className="arch_container">
+        <div className="arch_hero_section">
+          <h1>Architectural Design</h1>
+        </div>
+        <div className="arch_section_info">
+          <h1>Our Advantages in Architectural Design</h1>
+          <div className="info_para_handler">
+            <div className="info_para_1">
+              <h2>01: Modern Architecture</h2>
+              <h6>
+                This architectural style is an umbrella term that encompasses
+                several different styles that became prominent during the first
+                half of the 20th century.
+              </h6>
+              <h2>02: Victorian Architecture</h2>
+              <h6>
+                This style of architecture refers to buildings that were
+                constructed during the reign of England’s Queen Victoria.
+              </h6>
+            </div>
+            <div className="info_para_2">
+              <h2>04:Spanish Architecture</h2>
+              <h6>
+                Blending influences from Spain, Mexico, and Indigenous cultures.
+                beautiful architectural style has become a popular choice.
+              </h6>
+              <h2>05:Roman Classical Architecture</h2>
+              <h6>
+                This type of architecture refers to the style that was
+                prominently used in ancient Greece and Rome.
+              </h6>
+            </div>
+          </div>
+        </div>
+        <div className="arch_section_card">
+          <div className="arch_cards_controller">
+            <div className="arch_cards">
+              <h1>
+                High- End Architectural Design Services By Glorious Builders
+              </h1>
+              <p>
+                As stated before, we ensure extraordinary profile employees that
+                prepare a collection of competences of the home architects in
+                Carlifornia. This allows us to let you have a varied range of
+                architectural outcomes. We work in a way that holds over quite a
+                lot of inventive objects and equipment. Also, we contain the
+                idea of our consumer to expand the whole thing we are going to
+                work on. This is an excessive assistance while operational as we
+                design combined with satisfying customer’s requirements.
+                Glorious Builders is a service-oriented architectural firm in
+                Carlifornia that does not reflect whether the location is
+                residential, non-profit, commercial, or any other kind of land.
+                We undoubtedly recognize that a construction is just not
+                restricted to a structure. It must be in an open area that
+                increases the experience of every person using it along with
+                individuals receiving advantage from it in the environmental
+                characteristic. Glorious Builders got an all-inclusive method to
+                the complete procedure of designing. Our committed team just not
+                outfits what the consumer says, but they also abide by the
+                agenda and economical limitations of the consumer. This is what
+                finishes up with the establishment of buildable outcomes joined
+                up with artistic designs. Our designing team has faith that
+                designing is not just a painting. It is an exclusive blend of
+                visualization, dream, approach, and problem outcomes. These
+                aspects let us set the establishment and functionality to finish
+                up producing developments to bump into business requirements and
+                refining processes. We initiate development tactically and end
+                up design formation with graphic arts and pleasing designs
+              </p>
+            </div>
+            <div className="arch_cards">
+              <h1>We Design To Strengthen Your Identity</h1>
+              <p>
+                Glorious Builders among the construction companies in
+                Carlifornia proposes contemporary architectural and interior
+                design facilities for industrial, commercial and established
+                buildings design also with the best home architects in
+                Carlifornia within the client’s budget limitations. We ensure a
+                great assortment for planning and building wonderful, durable,
+                convenient and comfortable buildings by creating new concepts of
+                designs into the seamless solid construction. Our crew of
+                architectural designers is composed of professional and
+                knowledgeable experts devoted to conveying quality end products
+                and facilities within the decided time structure. Glorious
+                Builders is the best architecture firm in Carlifornia because we
+                provide state-of-the-art design-build facilities while obeying
+                to business verified best follows. We have the know-how, kit,
+                and top mark methods to take your visualization from napkin
+                outline to actual representativeness. We have accomplished a
+                high repute for proficiency, excellence and competence amongst
+                several of the construction companies in Carlifornia.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 };
