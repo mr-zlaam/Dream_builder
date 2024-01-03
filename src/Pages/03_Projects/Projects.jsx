aa5be5163625e4b0579e0e43f82058b8e2461fa8
Zlaam <mrzalaam@gmail.com>
2024-01-03 19:10:25 +0500

    last stable commit

--- src/Pages/03_Projects/Projects.jsx
+++ src/Pages/03_Projects/Projects.jsx
@@ -1,9 +1,22 @@
 import {} from "react";
-
+import "./Projects.scss";
+import data from "./Projects.json";
 const Projects = () => {
   return (
     <>
-      <div>Projects Pages</div>
+      <div className="project_container">
+        <h1>Our Latest Work</h1>
+        <div className="projects_controller">
+          {data &&
+            data.map((imageData) => {
+              return (
+                <div className="project_card" key={imageData.id}>
+                  <img src={imageData.url} alt="image" />
+                </div>
+              );
+            })}
+        </div>
+      </div>
     </>
   );
 };
