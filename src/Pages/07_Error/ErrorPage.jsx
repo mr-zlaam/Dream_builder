f511436193d3be348b2b8c10ecdb1bd854e21686
Zlaam <mrzalaam@gmail.com>
2024-01-04 12:01:08 +0500

    adding the error page

--- src/Pages/07_Error/ErrorPage.jsx
+++ src/Pages/07_Error/ErrorPage.jsx
@@ -1,9 +1,15 @@
 import {} from "react";
-
+import "./Error.scss";
+import { Link } from "react-router-dom";
 const Error = () => {
   return (
     <>
-      <div>Errorpage</div>
+      <div className="error_container">
+        <h1>Error 404: page not found</h1>
+        <p>
+          <Link to={"/"}>Home</Link>
+        </p>
+      </div>
     </>
   );
 };
